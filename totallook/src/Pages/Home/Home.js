fa8cb50a648674761aef45d51347986385c9adf3
Alejandra García <agarcia_pacheco@hotmail.com>
2021-05-20 15:44:04 -0500

    tratando de solucionar el problema

--- totallook/src/Pages/Home/Home.js
+++ totallook/src/Pages/Home/Home.js
@@ -1,16 +1,28 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState, useEffect} from 'react';
 import { Link } from "react-router-dom";
 
 const Home = () => {
+
+	const [clothes, setClothes] = useState([]);
+
+  const getData = async () => {
+    const data = await fetch("http://localhost:3001/products");
+    const dataJson = await data.json();
+    console.log(dataJson);
+    setClothes(dataJson);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
     return (
 			<Fragment>
 				<h3>Hola Andy</h3>
 				<Link to="/categories">
 					<p>Mostrar todas las categorías</p>	
 				</Link>	
-			</Fragment>
-
-        
+			</Fragment>       
 
     )
 
