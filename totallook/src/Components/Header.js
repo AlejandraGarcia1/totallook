94797d3dfe03e726e80603ffb42d327c5a6efd68
Alejandra García <agarcia_pacheco@hotmail.com>
2021-05-19 23:02:57 -0500

    feat/routerLayout

---
+++ totallook/src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
+const Header = () => {
+    return (
+        <div>
+            <p>Aqui va el logo</p>
+            <p>LogIn</p>
+            <ShoppingCartIcon />
+        </div>
+        
+    );
+}
+
+export default Header;
---
+++ totallook/src/Pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React, {Fragment} from 'react';
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (
+			<Fragment>
+				<h3>Hola Andy</h3>
+				<Link to="/categories">
+					<p>Mostrar todas las categorías</p>	
+				</Link>	
+			</Fragment>
+
+        
+    )
+
+};
+
+export default Home;
