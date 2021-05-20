46dd1e5966e704575eb3af0bffc993da47a2f3f8
Yessica <alvaradoyessica03@gmail.com>
2021-05-19 20:46:43 -0500

    router layout

--- totallook/src/App.js
+++ totallook/src/App.js
@@ -1,12 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+
+import Home from './Pages/Home/Home'
+import Product from './Pages/Product/Product'
+import ListProducts from './Pages/ListProducts/ListProducts'
+import Categories from './Pages/Categories/Categories'
+import Shopping from './Pages/ShoppingCart/ShoppingCart'
+import Header from './Components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <h1>HOLA</h1>
-    </div>
+
+      <Router>
+        <div>
+          <Header />
+          <div className="btn-group">					
+            <Link to="/" className="btn btn-dark">
+            Inicio
+            </Link>
+            <Link to="/contacto" className="btn btn-dark">
+            Contacto
+            </Link>					
+            <NavLink to="/nosotros" className="btn btn-dark" activeClassName="active">
+            Nosotros
+            </NavLink>
+          </div>
+        </div>
+        <hr/>		
+        <Switch>
+        <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/categories">
+            <Categories />
+          </Route>
+          <Route path="/listproducts">
+            <ListProducts />
+          </Route>
+          <Route path="/product">
+            <Product />
+          </Route>
+          <Route path="/shoppingcart">
+            <Shopping />
+          </Route>
+        </Switch>
+      </Router>
+    
   );
-}
+};
 
 export default App;
