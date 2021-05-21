ffd4bd13d183948a47422c3ad82eb50e2e212f15
Alejandra García <agarcia_pacheco@hotmail.com>
2021-05-20 20:37:24 -0500

    productos filtrados en lista de productos

--- totallook/src/Pages/Categories/Categories.js
+++ totallook/src/Pages/Categories/Categories.js
@@ -5,24 +5,6 @@ import { Link } from "react-router-dom";
 const Categories = () => {
 	const data = ProductData;
 	console.log('categorias',data.category)
-
-		const [dataFiltrada, setDataFiltrada] = useState([])
-
-		const getBolsas = (e) => {			
-			const data = ProductData;
-			
-			console.log(e)
-			console.log('categorias',data)
-			if(e.target.innerText === "Bolsas" ){
-				setDataFiltrada(data.filter(category => category.category === "bolsas"))
-			}else if(e.target.innerText === "Zapatos"){
-				setDataFiltrada(data.filter(category => category.category === "zapatos"))
-			}else if(e.target.innerText === "Ropa"){
-				setDataFiltrada(data.filter(category => category.category === "ropa"))
-			}
-			console.log(setDataFiltrada)
-				// data.map((item) => <li key={item.id}> {item.category === "bolsas"}</li>)			
-		}
 		
 		
 
@@ -31,13 +13,11 @@ const Categories = () => {
 			<h3>Categorías</h3>		
 			<p>Botones</p>	
 
-			<button>Ropa</button>
+			<button>Bolsas</button>
 			<button>Zapatos</button>
 			<button>
-				<Link to="/listproducts" onClick={getBolsas} >Bolsas </Link>
-				{
-					data.map((item) => <li key={item.id}> {item.category === "bolsas"}</li>)	
-				}
+				<Link to="/listproducts" >Ropa </Link>
+				
 			</button>
 			
 
--- totallook/src/Pages/ListProducts/ListProducts.js
+++ totallook/src/Pages/ListProducts/ListProducts.js
@@ -1,17 +1,32 @@
 import React, {Fragment} from 'react';
-// import {ProductData} from '../../Data/ProductsData';
+import {ProductData} from '../../Data/ProductsData';
 
 
+const ListProducts = () => {
+	const data = ProductData;	
+	console.log(data)
 
-const ListProducts = ({getBolsas}) => {
+	// const getRopa = (e) => {			
+	// 	const data = ProductData;		
+	// 	console.log(e)
+	// 	console.log('categorias',data)
+	// 	if(e.target.innerText === "Ropa" ){
+	// 		return(data.filter(category => category.category === "ropa"))
+	// 	}
+			
+	// }
 	// const data = ProductData
-	console.log(getBolsas)
+	
 
     return (
 			<Fragment>
-				<h3>List Products</h3>				
-				{/* {data && //&& para saber si existe
-        data.map((item) => <li key={item.id}> {item.product} </li>)}				 */}
+				<h3>List Products</h3>
+				<div>				
+				{data && //&& para saber si existe
+        data.map((item) => <div key={item.id}> <img src={item.image} alt="image"/> <h4>{item.product}</h4> {item.price} </div>)}
+
+				</div>
+						
 			</Fragment>
         
 			
